87c194a61c8032e6948619276121e682efc74638
Alex Birkett <alex@birkett.co.uk>
2012-01-05 21:27:02 +0100

    Udpated copyright notices

--- src/com/commonsware/cwac/locpoll/LocationPoller.java
+++ src/com/commonsware/cwac/locpoll/LocationPoller.java
@@ -1,5 +1,6 @@
 /***
 	Copyright (c) 2010 CommonsWare, LLC
+	Copyright 2011-2012 Birkett Enterprise Ltd
 	
 	Licensed under the Apache License, Version 2.0 (the "License"); you may
 	not use this file except in compliance with the License. You may obtain
